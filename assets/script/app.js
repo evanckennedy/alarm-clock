0f85bed9bc869fdf65206b137a62cb8b913061bd
Evan Kennedy <evanckennedy@protonmail.com>
2024-03-18 15:19:18 -0500

    prevent user from inputting anything other than numbers

--- assets/script/app.js
+++ assets/script/app.js
@@ -12,8 +12,11 @@ const displayTime = utils.select('h1');
 const hourInput = utils.select('.hour-input');
 const minuteInput = utils.select('.minute-input');
 const setAlarmButton = utils.select('.set-alarm-button');
-const alarmDisplay = utils.select('.alarm-display')
+const alarmDisplay = utils.select('.alarm-display');
 
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*  Alarm                                                */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 function getCurrentTime() {
   const now = new Date();
   const currentHour = formattedTime(now.getHours()); 
@@ -29,12 +32,19 @@ function formattedTime(time) {
   return String(time).padStart(2, '0'); // Adds 0 to one digit num
 }
 
-
-utils.listen('click', setAlarmButton, () => {
+function getUserTime() {
   const userHourInput = formattedTime(hourInput.value);
   const userMinuteInput = formattedTime(minuteInput.value);
   alarmDisplay.innerHTML = `${userHourInput}:${userMinuteInput}`;
-});
+}
+
+utils.listen('click', setAlarmButton, getUserTime);
+
+function enforceNumericInput(event) {
+  event.target.value = event.target.value.replace(/[^0-9]/g, '');
+}
+utils.listen('input', hourInput, enforceNumericInput);
+utils.listen('input', minuteInput, enforceNumericInput);
 
 
 
